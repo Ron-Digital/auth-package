5ae452340677b7a8db3b767ddfd4ffeac5eb8dc1
Ceren Özkurt <33186246+cerenozkurt@users.noreply.github.com>
2023-06-07 23:54:45 +0300

    added register func

--- src/AuthService.php
+++ src/AuthService.php
@@ -10,7 +10,7 @@ class AuthService
     protected $project;
     public function __construct($projectName)
     {
-        $this->authUrl = "https://auth.ronservice.co";
+        $this->authUrl = "http:/localhost:8001";
         $this->project = \config('authservice.project_name') ?? $projectName;
     }
     public function login($userName, $password)
@@ -19,6 +19,66 @@ class AuthService
             'username' => $userName,
             'password' => $password
         ]);
+        if ($loginResponse->getStatusCode() != 200) {
+            return $loginResponse->json();
+        }
+        $responseData = json_decode($loginResponse->body(), true);
+        return $responseData;
+    }
+
+    public function register(
+        $email = null,
+        $password,
+        $userName = null,
+        $givenName = null,
+        $familyName = null,
+        $name = null,
+        $nickName = null,
+        $picture = null,
+        $userData = []
+    ) {
+        $details = [];
+        foreach ($userData as $key => $value) {
+            $details[$key] = $value;
+        }
+        $query = [
+            'password' => $password
+        ];
+
+        if ($email) {
+            $query['email'] = $email;
+        }
+
+        if ($userName) {
+            $query['username'] = $userName;
+        }
+
+        if ($givenName) {
+            $query['given_name'] = $givenName;
+        }
+
+        if ($familyName) {
+            $query['family_name'] = $familyName;
+        }
+
+        if ($name) {
+            $query['name'] = $name;
+        }
+
+        if ($nickName) {
+            $query['nick_name'] = $nickName;
+        }
+
+        if ($picture) {
+            $query['picture'] = $picture;
+        }
+        if ($details != []) {
+            $query['user_data'] = $details;
+        }
+
+        // Diğer alanları buraya ekleyebilirsiniz
+
+        $loginResponse = Http::post($this->authUrl . "/register?project=" . $this->project, $query);
         if ($loginResponse->getStatusCode() != 200) {
             return $loginResponse->json();
         }
